5d22d9817a18baaa4bc3aac61dafaef3894fa750
Matt <mjbresett@gmail.com>
2018-02-14 11:32:41 -0500

    added refresh buttons

--- betterGui.py
+++ betterGui.py
@@ -16,14 +16,29 @@ class gui:
 
         self.winHeight = 600
         self.winWidth = 1200
+        master.geometry(str(self.winWidth) + 'x' + str(self.winHeight))
+
         self.cursor = dbConnect.connect().cursor()
+
         self.colors = colors = ["#2EF004", "#5DEF03", "#8AEE03", "#B8ED02",
             "#E5EB02", "#EAC201", "#E99301", "#E76500", "#E63600", "#E50800"]
 
+        self.imageLoc = "testImages/hl_plants_20171117.jpg"
+        self.graphLoc = "1"
+
         self.tabs = self.form_notebook()
-        self.grid_db_data(self.tabs[0])
-        self.draw_graph(self.tabs[1], "1")
-        self.draw_image("testImages/hl_plants_20171117.jpg", self.tabs[2])
+
+        self.grid_db_data()
+        refOne = tk.Button(self.tabs[0], text="Refresh", command=self.refreshOne)
+        refOne.place(relx=.9, rely=.9)
+
+        self.draw_graph()
+        refTwo = tk.Button(self.tabs[1], text="Refresh", command=self.refreshTwo)
+        refTwo.place(relx=.9, rely=.9)
+
+        self.draw_image()
+        refThree = tk.Button(self.tabs[2], text="Refresh", command=self.refreshThree)
+        refThree.place(relx=.9, rely=.9)
 
 
     def form_notebook(self):
@@ -41,7 +56,7 @@ class gui:
 
         return[tab1, tab2, tab3]
 
-    def grid_db_data(self, frame):
+    def grid_db_data(self):
         #get grid data
         self.cursor.execute('call get_grid_info()')
         info = self.cursor.fetchall()
@@ -72,15 +87,15 @@ class gui:
             for j in range(width):
                 ind = str(i)+ "," + str(j)
                 if ind in names:
-                    b = tk.Label(frame, text=names[ind]["plant"],
+                    b = tk.Label(self.tabs[0], text=names[ind]["plant"],
                         bg=names[ind]["bg"], relief="solid", width=maxChar)
                     b.grid(row=i, column=j)
                     relationx =  (j-midx)*.1+.5
                     relationy =  (i-midy)*.075+.5
                     b.place(relx=relationx, rely=relationy, anchor="c")
 
-    def get_measurements(self, location_id):
-        self.cursor.execute('call get_ndvi_over_time(' + str(location_id) + ')')
+    def get_measurements(self):
+        self.cursor.execute('call get_ndvi_over_time(' + str(self.graphLoc) + ')')
         info = self.cursor.fetchall()
         times = []
         vals = []
@@ -90,26 +105,41 @@ class gui:
             vals.append(measurement['ndvi_val'])
         return [times, vals]
 
-    def draw_graph(self, tab, location_id):
+    def draw_graph(self):
         f = Figure(figsize=(5,5), dpi=100)
         a = f.add_subplot(111)
-        data = self.get_measurements(location_id)
+        data = self.get_measurements()
         a.plot(data[0], data[1])
 
-        canvas = FigureCanvasTkAgg(f, tab)
+        canvas = FigureCanvasTkAgg(f, self.tabs[1])
         canvas.show()
         canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
 
-        toolbar = NavigationToolbar2TkAgg(canvas, tab)
+        toolbar = NavigationToolbar2TkAgg(canvas, self.tabs[1])
         toolbar.update()
         canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-    def draw_image(self, location, tab):
-        if location is not None:
-            pil_img = Image.open(location)
+    def draw_image(self):
+        if self.imageLoc is not None:
+            pil_img = Image.open(self.imageLoc)
             self.img = ImageTk.PhotoImage(pil_img)
-            l = tk.Label(tab, image=self.img)
+            l = tk.Label(self.tabs[2], image=self.img)
             l.grid(row=1, column=1)
+
+    def refreshOne(self):
+        self.tabs[0].grid_forget()
+        self.grid_db_data()
+        return 1
+
+    def refreshTwo(self):
+        self.tabs[1].grid_forget()
+        self.draw_graph()
+        return 1
+
+    def refreshThree(self):
+        self.tabs[2].grid_forget()
+        self.draw_image()
+        return 1
 
 root = tk.Tk()
 my_gui = gui(root)
