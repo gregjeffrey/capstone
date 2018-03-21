8b89f457175de9048adf57a231916fc9aa46ddfd
Greg Jeffrey <gjeffs11@gmail.com>
2018-03-21 14:40:18 -0400

    Communication is functional!

--- bbmaster.py
+++ bbmaster.py
@@ -3,92 +3,92 @@ from serial import Serial
 
 
 def send_msg(fmtstr,msg,port):
-	try:
-		#Stage 1: Send Length of message's format string
-		#But it has to send it as an ascii string (and not an int) because fuck you
-		fmtstrsize = len(fmtstr)
-		numstr = '%03d' % fmtstrsize
-		stg1_msg = struct.pack('@3s',numstr)
-		port.write(stg1_msg)
-		
-		#Stage 2: Send The message's format string
-		stg2_fmtstr = '@%is' % (fmtstrsize)
-		stg2_msg = struct.pack(stg2_fmtstr,fmtstr.encode())
-		port.write(stg2_msg)
-		
-		#Stage 3: Send The data
-		stg3_msg = struct.pack(fmtstr,*msg)
-		port.write(stg3_msg)
-		return 1
-	except:
-		return -1
-
+	#Stage 1: Send Length of message's format string
+	#But it has to send it as an ascii string (and not an int) because fuck you
+	fmtstrsize = len(fmtstr)
+	numstr = '%03d' % fmtstrsize
+	stg1_msg = struct.pack('@3s',numstr)
+	port.write(stg1_msg)
 	
-
+	#Stage 2: Send The message's format string
+	stg2_fmtstr = '@%is' % (fmtstrsize)
+	stg2_msg = struct.pack(stg2_fmtstr,fmtstr)
+	port.write(stg2_msg)
 	
+	#Stage 3: Send The data
+	stg3_msg = struct.pack(fmtstr,*msg)
+	port.write(stg3_msg)
+	
+	return 1
+
 ## Note: On the Beaglebone (in pyserial), the read() function is blocking
 ## I.E. It will read until the serial port's timeout is reached, OR
 ## until the given number of bytes have been read.
 def recv_msg(port):
-	try:
-		#Receive stage 1: Size of the message's format string
-		fmtstr_size = struct.unpack('@i',port.read(4))[0]
+	#Receive stage 1: Size of the message's format string
+	fmtstr_size = struct.unpack('@i',port.read(4))[0]
+	
+	#Receive stage 2: The message's format string
+	numstr = '@%is' % fmtstr_size
+	fmtstr = struct.unpack(numstr,port.read(fmtstr_size))[0]
 
-		#Receive stage 2: The message's format string
-		fmtstr = struct.unpack('@s',port.read(fmtstr_size))[0]
+	#Receive stage 3: The data/message
+	datasize = struct.calcsize(fmtstr)
+	data = struct.unpack(fmtstr,port.read(datasize))
 
-		#Receive stage 3: The data/message
-		datasize = struct.calcsize(fmtstr)
-		data = struct.unpack(fmtstr,port.read(datasize))
-
-		#Send receipt confirmation
-		port.write(struct.pack('@i',1))
+	if data:
 		return data
-	except:
-		port.write(struct.pack('@i',0))
+	else:
 		return -1
 
+		
+def recv_img(port,img_no):
 
-def send_trigger(prt):
-	trigger = (b'Nick Sucks',)
-	success = send_msg('@10s',trigger,prt) #Sends initialization trigger to the camera		
+	sz = struct.unpack('@i',port.read(4))[0]
+	print 'Reading %i bytes: ' % sz
+	img = port.read(sz)
+	
+	#Write the image to a file
+	filename = 'testimg%i.jpg' % img_no
+	imgfile = open(filename,'w')
+	imgfile.write(img)
+	imgfile.close()
+	return imgfile
+
+			
+def send_trigger(port):
+	trigger = (b'Go',)
+	success = send_msg('@2s',trigger,port) #Sends initialization trigger to the camera		
+	return success
+	
+def send_averages(port):
+	fakedata = (1,2,3,4,5,6,b'Test String')
+	success = send_msg('@6i11s',fakedata,port)
 	return success
 
+
+#Set up PySerial connection	
 try:
-	prt = Serial(port='/dev/ttyACM0',baudrate=9600,timeout=5)
-	#prt = Serial(port='COM7',baudrate=9600,timeout=5)
-except:
-	prt = Serial(port='/dev/ttyACM1',baudrate=9600,timeout=5)
-	pass
+	port = Serial(port='/dev/ttyACM0',baudrate=115200,timeout=5)
+	port.inWaiting()
+except e:
+	print e
+	port = Serial(port='/dev/ttyACM1',baudrate=115200,timeout=5)	
 
-#send_trigger(prt)
+	
+def mainloop(num):
+	prt.flushInput()
+	send_trigger(port)
+	send_averages(port)
+	data = recv_msg(port)
+	print data
+	imgfile = recv_img(port,num)
+	print 'Image saved at %s' % imgfile
+	return imgfile
 
 
-##	if success:
-##		#Sends moving averages of data to the camera
-##		fmtstr = "@3i50p" #six integers and one 'Pascal' string of a fixed length (50 bytes)
-##		data_tup = (1,2,3,b'Nick Sucks')
-##		success = send_msg(fmtstr,data_tup,prt)
-##		
-##		#Waits for calculated data to be returned
-##		#Uses same format string as the sent moving averages
-##		data = recv_msg(prt)
-##		
-##		#Waits for image size to be returned
-##		fmtstr = "@i"
-##		msg = prt.read(struct.calcsize(fmtstr))
-##		imgsize = struct.unpack(fmtstr,msg)[0]
-##
-##		#Waits for image to be sent back,based on calculated image size
-##		img = prt.read(imgsize)
-##
-##		#Saves image to file
-##		#filename = 'testimg.jpg'
-##		#imgfile = open(filename,'w')
-##		#imgfile.write(img)
-##		#imgfile.close()
-##
-##		return img
+	
+
                 
 
 
--- camera_master.py
+++ camera_master.py
@@ -6,70 +6,125 @@ import sensor, image, time, pyb,ustruct
 #Stage 2: the message's format string as a str
 #Stage 3: the message itself as a tuple
 #Assumes msg is a tuple of the form (x1,x2,...,xn) with any strings encoded as bytes and no "nested" tuples
+#EVEN IF YOU ARE JUST SENDING ONE STRING, PUT IT IN A TUPLE BY ITSELF
 #fmtstr is a str
 #Condition data as such before calling the function
-
 def send_msg(fmtstr,msg,port):
 	try:
-		#Stage 1: Send Length of message's format string
+		#Stage 1: Length of message's format string
 		fmtstrsize = len(fmtstr)
 		stg1_msg = ustruct.pack('@i',fmtstrsize)
+		
+		stg2_fmtstr = '@%is' % (fmtstrsize) # Format-stringception
+		stg2_msg = ustruct.pack(stg2_fmtstr,fmtstr.encode()) #Stage 2: Send The message's format string
+		
+		stg3_msg = ustruct.pack(fmtstr,*msg) #Stage 3: The data tuple
+		
+		#Write the message(s) to the port
 		port.write(stg1_msg)
-		
-		#Stage 2: Send The message's format string
-		stg2_fmtstr = '@%is' % (fmtstrsize)
-		stg2_msg = ustruct.pack(stg2_fmtstr,fmtstr.encode())
 		port.write(stg2_msg)
-		
-		#Stage 3: Send The data
-		stg3_msg = ustruct.pack(fmtstr,*msg)
 		port.write(stg3_msg)
+		
 		return 1
 	except:
 		return -1
 	
-#Best way to read a line from the USB_VCP
-def getln(port):
-	while(1):
-		if port.any():
-			msg = port.readline()
-			port.write(ustruct.pack('@i',1))
-			break
-	return msg #Returns raw bytes message
-	
 def recv_msg(port):
-
-	stg1 = getln(port)
-	fmtstr_size = ustruct.unpack('@3s',stg1)[0] # Receive stage 1- the size of the format string
-	fmtstr_size = int(fmtstr_size.decode()) #Turns it back into an actual fucking number
-
-	fmtstringception = '@%is' % fmtstr_size # The format string of the format string = fmtstringception
-	#Receive stage 2 - the format string
-	stg2 = getln(port)
-	fmtstr = ustruct.unpack(fmtstringception,stg2)[0]
-	fmtstr = fmtstr.decode()
+	#Best way to read a line from the USB_VCP
 	
-	#Receive stage 3 - the Data
-	stg3 = getln(port)
-	data = ustruct.unpack(fmtstr,stg3)
-			
-	return data
+	def getln():
+		while(1):
+			if port.any():
+				msg = port.readline()
+				break
+		return msg #Returns raw bytes message
+		
+	try:
+		stg1 = getln()
+		fmtstr_size = ustruct.unpack('@3s',stg1)[0] # Receive stage 1- the size of the format string
+		fmtstr_size = int(fmtstr_size.decode()) #Turns it back into an actual fucking number
 
+		fmtstringception = '@%is' % fmtstr_size # The format string of the format string = fmtstringception
+		#Receive stage 2 - the format string
+		stg2 = getln()
+		fmtstr = ustruct.unpack(fmtstringception,stg2)[0]
+		fmtstr = fmtstr.decode()
+		
+		#Receive stage 3 - the Data
+		stg3 = getln()
+		data = ustruct.unpack(fmtstr,stg3)
+				
+		return data
+		
+	except:
+		return -1
+
+	
+def send_img(img,port):
+	try:
+		jpg = img.compressed() # Creates jpg byte object
+		sz = jpg.size()
+		packed_size = ustruct.pack('@i',sz)
+		port.write(packed_size)
+		port.send(jpg)
+		return 1
+	except:
+		return -1
+	
+	
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time=2000)
 clock = time.clock()
+
 brd = pyb.USB_VCP()
 brd.setinterrupt(-1)
-led1 = pyb.LED(1)
 
-#Listen for the command/trigger
-while(brd.isconnected()):
-	#Looks for initialization command
-	cmd = recv_msg(brd)[0]
-	if cmd.decode() == 'Nick Sucks':
-		led1.on()
+led1 = pyb.LED(1)
+led2 = pyb.LED(2)
+
+triggerstring = 'Go'
+
+while(1):
+	if brd.isconnected(): # Listen for the command/trigger
+		
+		#Does a few flashes if the USB is connected
+		for i in range(3):
+			led2.on()
+			time.sleep(500)
+			led2.off()
+		
+		
+		cmd = recv_msg(brd)[0].decode() # Looks for initialization command
+		if cmd == triggerstring:
+			led1.on()
+			
+			avg_data = recv_msg(brd) #Receive calibration data/moving averages from BB
+			if avg_data != -1:
+				led2.on()
+			
+			##############################################
+			### Camera People - Do all your shit here! ###
+			##############################################
+			
+			img = sensor.snapshot()
+			
+			#Format data to be sent back to the Beaglebone
+			#see send_msg() function for description
+			#As long as it is formatted as such, BB can handle it (I promise) (i hope...)
+			msg = (1,b'Nick Sucks',2,b'Calvin Sucks Too')
+			fmtstr = '@i10si16s'
+			
+			#Send the image
+			success = send_msg(fmtstr,msg,brd)
+			imgsuccess = send_img(img,brd)
+			time.sleep(1000)
+			
+			led1.off()
+			led2.off()
+		
+		
 
 		
                                         
